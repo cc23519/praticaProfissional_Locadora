35f9689ad5c6b16a675b2fddd5bc38d93337005c
Guilherme da Silva Pereira <cc23519@g.unicamp.br>
2023-11-15 09:40:10 -0300

    modificações na classe de cliente
    preparação para a tela de alterações conseguir pegar os dados corretos do cliente. adicionado um construtor, os setters e getters para as constantes rua, numero, cidade, bairro, estado, cep e complemento.

--- projetoJava/locadora/src/main/java/com/locadora/locadora/Cliente.java
+++ projetoJava/locadora/src/main/java/com/locadora/locadora/Cliente.java
@@ -1,77 +1,157 @@
 package com.locadora.locadora;
 
 public class Cliente {
-        private int id;
-        private String nome;
-        private String cpf;
-        private String tipoTelefone;
-        private String telefone;
-        private String endereco1;
-        private String endereco2;
 
-        public Cliente(int id, String nome, String cpf, String tipoTelefone, String telefone, String endereco1, String endereco2) {
-            this.id = id;
-            this.nome = nome;
-            this.cpf = cpf;
-            this.tipoTelefone = tipoTelefone;
-            this.telefone = telefone;
-            this.endereco1 = endereco1;
-            this.endereco2 = endereco2;
-        }
+    private int id;
+    private String nome;
+    private String cpf;
+    private String tipoTelefone;
+    private String telefone;
+    private String endereco1;
+    private String endereco2;
 
-        public int getId() {
-            return id;
-        }
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String complemento;
 
-        public String getNome() {
-            return nome;
-        }
-
-        public String getCpf() {
-            return cpf;
-        }
-
-        public String getTipoTelefone() {
-            return tipoTelefone;
-        }
-
-        public String getTelefone() {
-            return telefone;
-        }
-
-        public String getEndereco1() {
-            return endereco1;
-        }
-
-        public String getEndereco2() {
-            return endereco2;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public void setCpf(String cpf) {
-            this.cpf = cpf;
-        }
-
-        public void setTipoTelefone(String tipoTelefone) {
-            this.tipoTelefone = tipoTelefone;
-        }
-
-        public void setTelefone(String telefone) {
-            this.telefone = telefone;
-        }
-
-        public void setEndereco1(String endereco1) {
-            this.endereco1 = endereco1;
-        }
-
-        public void setEndereco2(String endereco2) {
-            this.endereco2 = endereco2;
-        }
+    public Cliente(int id, String nome, String cpf, String tipoTelefone, String telefone, String endereco1, String endereco2) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.tipoTelefone = tipoTelefone;
+        this.telefone = telefone;
+        this.endereco1 = endereco1;
+        this.endereco2 = endereco2;
     }
+
+    public Cliente(int id, String nome, String cpf, String tipoTelefone, String telefone, String rua, String numero, String bairro, String cidade, String estado, String cep, String complemento) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.tipoTelefone = tipoTelefone;
+        this.telefone = telefone;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.complemento = complemento;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTipoTelefone() {
+        return tipoTelefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEndereco1() {
+        return endereco1;
+    }
+
+    public String getEndereco2() {
+        return endereco2;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setTipoTelefone(String tipoTelefone) {
+        this.tipoTelefone = tipoTelefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEndereco1(String endereco1) {
+        this.endereco1 = endereco1;
+    }
+
+    public void setEndereco2(String endereco2) {
+        this.endereco2 = endereco2;
+    }
+}
