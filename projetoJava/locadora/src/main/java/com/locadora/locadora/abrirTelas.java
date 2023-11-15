c4829cc5c4f54ac020f56b6fc5a4ca3570239b32
Guilherme da Silva Pereira <cc23519@g.unicamp.br>
2023-11-15 15:39:10 -0300

    criação de novas subclasses para abrir especificamente as telas de exclusão
    Classes abrirTelaComDadosCarro, Seguros e Cliente.

--- projetoJava/locadora/src/main/java/com/locadora/locadora/abrirTelas.java
+++ projetoJava/locadora/src/main/java/com/locadora/locadora/abrirTelas.java
@@ -1,5 +1,8 @@
 package com.locadora.locadora;
 
+import com.locadora.locadora.controladoresAlterações.telaCarroController;
+import com.locadora.locadora.controladoresAlterações.telaClienteController;
+import com.locadora.locadora.controladoresAlterações.telaSeguroController;
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,10 +10,68 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class abrirTelas {
+
     public void abrirTela(String nomedocumento, String titulo) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(nomedocumento));
             Parent root = loader.load();
+            Stage novaTela = new Stage();
+            novaTela.setTitle(titulo);
+            novaTela.setScene(new Scene(root));
+            novaTela.setResizable(false);
+            novaTela.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void abrirTelaComDadosCarro(String nomedocumento, String titulo, Object... dadosCarro) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(nomedocumento));
+            Parent root = loader.load();
+
+            telaCarroController controlador = loader.getController();
+
+            controlador.inicializarDadosCarro(dadosCarro);
+
+            Stage novaTela = new Stage();
+            novaTela.setTitle(titulo);
+            novaTela.setScene(new Scene(root));
+            novaTela.setResizable(false);
+            novaTela.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void abrirTelaComDadosSeguros(String nomedocumento, String titulo, Object... dadosSeguro) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(nomedocumento));
+            Parent root = loader.load();
+
+            telaSeguroController controlador = loader.getController();
+
+            controlador.inicializarDadosSeguro(dadosSeguro);
+
+            Stage novaTela = new Stage();
+            novaTela.setTitle(titulo);
+            novaTela.setScene(new Scene(root));
+            novaTela.setResizable(false);
+            novaTela.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void abrirTelaComDadosCliente(String nomedocumento, String titulo, Object... dadosCliente) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(nomedocumento));
+            Parent root = loader.load();
+
+            telaClienteController controlador = loader.getController();
+
+            controlador.inicializarDadosSeguro(dadosCliente);
+
             Stage novaTela = new Stage();
             novaTela.setTitle(titulo);
             novaTela.setScene(new Scene(root));
