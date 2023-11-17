2fcb7cbac1ecbbb086920d85f41195e7901563f4
Guilherme da Silva Pereira <142462770+cc23519@users.noreply.github.com>
2023-11-17 20:13:52 -0300

    continuação do ultimo commit

--- projetoJava/locadora/src/main/java/com/locadora/locadora/controladoresAlterações/telaSeguroController.java
+++ projetoJava/locadora/src/main/java/com/locadora/locadora/controladoresAlterações/telaSeguroController.java
@@ -4,11 +4,32 @@ package com.locadora.locadora.controladoresAlterações;
 import com.locadora.locadora.Seguro;
 import com.locadora.locadora.usuario;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class telaSeguroController {
     
     private Integer id;
     private String tipo, preco, descricao, loginuser;
+    
+    @FXML
+    public TextField textfieldTipoSeguro;
+    
+    @FXML
+    public TextField textfielddescricaoSeguro;
+    
+    @FXML
+    public TextField textfieldPrecoSeguro;
+    
+    @FXML
+    public Button buttonFinalizarSeguro;
+    
+    @FXML
+    public Button buttonSair;
+    
+    @FXML
+    public Label statusSeguro;
     
     
     @FXML
