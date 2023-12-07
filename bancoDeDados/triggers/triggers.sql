8f7ff5ba6f33b48c3b613ea32d0008048383eef1
Guilherme da Silva Pereira <142462770+cc23519@users.noreply.github.com>
2023-12-06 21:18:07 -0300

    trigger corrigido
    Trigger testado e mais simples comparado com o anterior.

--- bancoDeDados/triggers/triggers.sql
+++ bancoDeDados/triggers/triggers.sql
@@ -4,15 +4,18 @@ AFTER INSERT
 AS
 BEGIN
 
-IF (CURRENT_TIMESTAMP >= NEW.dataFim)
+DECLARE @dataInicio DATE,
+      @dataFim DATE;
+
+SELECT @dataInicio = dataInicio, @dataFim = dataFim
+FROM inserted;
+
+IF (CURRENT_TIMESTAMP >= @dataFim)
 BEGIN
 UPDATE esquemaLocadora.tabelaLocacao_Ativa
 SET status = 'Finalizada'
-WHERE idLocacao = NEW.idLocacao;
-
-INSERT INTO esquemaLocadora.tabelaLocacao_Historico
-(FK_idClienteLocacao, FK_idSeguroLocacao, FK_idCarroLocacao, dataCriacao, dataInicio, dataFim, valorTotal, status)
-SELECT NEW.FK_idClienteLocacao, NEW.FK_idSeguroLocacao, NEW.FK_idCarroLocacao, NEW.dataCriacao, NEW.dataInicio, NEW.dataFim, NEW.valorTotal, NEW.status;
+WHERE idLocacao = (SELECT idLocacao FROM inserted);
 END;
 
 END;
+
